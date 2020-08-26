3585af5d6708e534c1d6ce89f5b807c2cd0ee90b
Jesus Francisco <jesusfranks6@gmail.com>
2020-08-26 15:47:46 -0500

    cambio

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,7 @@
 'use strict'
 /**
  * New Relic agent configuration.
- *
+ 
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
